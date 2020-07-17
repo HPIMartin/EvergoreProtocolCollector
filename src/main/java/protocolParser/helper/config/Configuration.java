d340186e5fd8814743be242383509c761d7af4e9
Martin <martinundpralli+github@gmail.com>
2020-07-17 21:46:30 +0200

    Add option for second server

--- src/main/java/protocolParser/helper/config/Configuration.java
+++ src/main/java/protocolParser/helper/config/Configuration.java
@@ -1,8 +1,12 @@
 package protocolParser.helper.config;
 
 public class Configuration {
-	public String browser = "gecko";
-	public int numberOfPages = 1;
+	public static final String ZYRTHANIA = "zyrthania";
+	public static final String IKANDUR = "ikandur";
+
+	public String browser = "chrome";
+	public int numberOfPages = 8;
 	public boolean parseStorage = false; // parses Bank if false
 	public boolean devMode = false;
+	public String server = ZYRTHANIA;
 }
