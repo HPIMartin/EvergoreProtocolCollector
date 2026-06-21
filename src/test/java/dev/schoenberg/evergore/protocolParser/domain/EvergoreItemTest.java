3b03e7154ce720bde3323fe0a71cf09a58661dcc
Martin <martinundpralli+github@gmail.com>
2026-06-21 13:18:58 +0000

    Add a golden-master test pinning every item's storage and withdrawl value

--- src/test/java/dev/schoenberg/evergore/protocolParser/domain/EvergoreItemTest.java
+++ src/test/java/dev/schoenberg/evergore/protocolParser/domain/EvergoreItemTest.java
@@ -2,7 +2,9 @@ package dev.schoenberg.evergore.protocolParser.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static dev.schoenberg.evergore.protocolParser.domain.EvergoreItem.Recipe.NOT_CRAFTABLE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 
 class EvergoreItemTest {
 	@Test
@@ -21,5 +23,33 @@ class EvergoreItemTest {
 	void calculatesStorageAndWithdrawlForGems() {
 		assertThat(EvergoreItem.KRISTALL.getStorageValue()).isZero();
 		assertThat(EvergoreItem.KRISTALL.getWithdrawlValue()).isEqualTo(300);
+	}
+
+	@Test
+	void withdrawlValueIsMarketValueScaledBy0Point6ForEveryItem() {
+		for (EvergoreItem item : EvergoreItem.values()) {
+			double expected = item.marketValue * 0.6d;
+			assertThat(item.getWithdrawlValue()).as("withdrawl value for %s", item.name()).isCloseTo(expected, within(0.0001d));
+		}
+	}
+
+	@Test
+	void storageValueIsZeroForAllNotCraftableItems() {
+		for (EvergoreItem item : EvergoreItem.values()) {
+			if (item.recipe == NOT_CRAFTABLE) {
+				assertThat(item.getStorageValue()).as("storage value for NOT_CRAFTABLE item %s", item.name()).isZero();
+			}
+		}
+	}
+
+	@Test
+	void storageValueIsSumOfIngredientWithdrawlCostsPerUnitForCraftableItems() {
+		for (EvergoreItem item : EvergoreItem.values()) {
+			if (item.recipe != NOT_CRAFTABLE) {
+				double expectedRecipeTotal = item.recipe.ingredients.stream().mapToDouble(ing -> ing.amount * ing.item.getWithdrawlValue()).sum();
+				double expected = expectedRecipeTotal / item.recipe.amount;
+				assertThat(item.getStorageValue()).as("storage value for craftable item %s", item.name()).isCloseTo(expected, within(0.0001d));
+			}
+		}
 	}
 }
