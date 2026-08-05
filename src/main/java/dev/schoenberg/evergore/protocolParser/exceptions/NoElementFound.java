c153c8d53792ad2ff4032ec9b533f46e83c7d816
Martin <martinundpralli+github@gmail.com>
2026-08-05 20:02:44 +0000

    Hide NoElementFound's requested value behind its message

--- src/main/java/dev/schoenberg/evergore/protocolParser/exceptions/NoElementFound.java
+++ src/main/java/dev/schoenberg/evergore/protocolParser/exceptions/NoElementFound.java
@@ -1,7 +1,7 @@
 package dev.schoenberg.evergore.protocolParser.exceptions;
 
 public class NoElementFound extends ProtocolParserException {
-	public final String requestedValue;
+	private final String requestedValue;
 
 	public NoElementFound(String requestedValue) {
 		this.requestedValue = requestedValue;
